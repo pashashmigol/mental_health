5178c58061d9cd81e91ae879a487a4addfa7cc85
ts-pavlo.shmyhol <pashashmigol@gmail.com>
2021-03-26 16:27:55 +0200

    hide Settings.kt
    Took 6 minutes

--- bot/src/main/kotlin/Settings.kt
+++
@@ -1,4 +0,0 @@
-object Settings {
-    const val MMPI_566_QUESTIONS_FILE_ID = "1ogoB6NjRtl5929sX8cRqWdyqCUq8hrR7P307NeaPZIc"
-    const val MMPI_377_QUESTIONS_FILE_ID = "1yx3hsC8E9UKDRJF0DjwnXTgf-QLQXXHk4_d2ZlMzoEk"
-}
